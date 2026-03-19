96dc08d3d2da0df72cfc56ed4f996b4122a803c8
Sergio Quintero <squintero@qpalliance.co>
2026-03-19 16:22:04 -0500

    feat: pantalla de bienvenida + sin limite 15 archivos

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-exec gunicorn --bind "0.0.0.0:${PORT:-8080}" --workers 1 --timeout 300 --log-level info app:app
+exec gunicorn --bind "0.0.0.0:${PORT:-8080}" --workers 1 --timeout 600 --log-level info --limit-request-fields 500 --limit-request-field_size 0 app:app
